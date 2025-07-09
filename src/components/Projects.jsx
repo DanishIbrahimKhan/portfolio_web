b7d4e1f2c8dd13ead36c48ce3037af35a48a8fef
Danish Ibrahim <mr.danishibrahim@gmail.com>
2025-07-09 09:33:24 +0530

    added a projects and try to solve a redirect issue to

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -39,7 +39,7 @@ export default function Card() {
         <div className="flex items-center justify-between mb-5">
           <div className="font-medium text-lg flex items-center gap-x-2">
             <div className="w-1.5 h-1.5 bg-gray-400 rounded-full"></div>
-            Projectss
+            Projects
           </div>
           <button
             onClick={() => navigate("/projects")}
